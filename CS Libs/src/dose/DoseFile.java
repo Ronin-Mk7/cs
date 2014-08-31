c8da2035aff74bc34a8d268a157815a1a1fac9dc
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-08-30 21:06:51 -0500

    Start a dose of dose

---
+++ CS Libs/src/dose/DoseFile.java
@@ -0,0 +1,10 @@
+package dose;
+
+/**
+ * @author Arthur Pachachura
+ * A file controlled by Dose.  Can be either local or inside a JAR.
+ * Either way, Dose functions identically.
+ */
+public class DoseFile {
+	
+}
---
+++ CS Libs/src/dose/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * DOSE - JAR File Management
+ */
+/**
+ * @author Arthur Pachachura
+ * DOSE - JAR File Management
+ */
+package dose;
